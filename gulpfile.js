2605e45a7b4f439679e767c9dbfddae028094d8e
Paul Shryock <paul@pshry.com>
2019-09-16 13:26:26 -0400

    Put src and dest paths into an object

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,21 @@ const { src, dest, series, parallel } = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const rename = require("gulp-rename");
 
+const defaults = {
+  html: {
+    src: './src/*.html',
+    dest: './build'
+  },
+  css: {
+    src: 'src/css/*.css',
+    dest: './build/css'
+  },
+  js: {
+    src: 'src/js/**/*.js',
+    dest: './build/js'
+  },
+}
+
 function clean() {
   const del = require('del');
 
@@ -11,14 +26,14 @@ function clean() {
 }
 
 function htmlBundle() {
-  return src('./src/*.html')
-    .pipe(dest('./build'))
+  return src(defaults.html.src)
+    .pipe(dest(defaults.html.dest))
 }
 
 function cssLint() {
   const gulpStylelint = require('gulp-stylelint');
 
-  return src('src/css/*.css')
+  return src(defaults.css.src)
     .pipe(gulpStylelint({
       config: {
         extends: ['stylelint-config-standard'],
@@ -32,7 +47,7 @@ function cssLint() {
 function cssBundle() {
   const postcss = require('gulp-postcss')
 
-  return src('src/css/*.css')
+  return src(defaults.css.src)
     .pipe( sourcemaps.init() )
     .pipe( postcss([
       require('postcss-easy-import'), // Concatenate
@@ -41,13 +56,13 @@ function cssBundle() {
       require('autoprefixer') // Add vendor prefixes
       ]) )
     .pipe( sourcemaps.write() )
-    .pipe( dest('./build/css') )
+    .pipe( dest(defaults.css.dest) )
     .pipe( postcss([
       require('cssnano') // Minify
       ]) )
     .pipe(rename({ suffix: '.min' }))
     .pipe(sourcemaps.write())
-    .pipe(dest('./build/css'))
+    .pipe(dest(defaults.css.dest))
 }
 
 /**
@@ -60,15 +75,15 @@ function jsBundle() {
   const concat = require('gulp-concat');
   const uglify = require('gulp-uglify');
 
-  return src('src/js/**/*.js')
+  return src(defaults.js.src)
     .pipe(sourcemaps.init())
     .pipe(concat('bundle.js')) // Concatenate
     .pipe(sourcemaps.write())
-    .pipe(dest('./build/js'))
+    .pipe(dest(defaults.js.dest))
     .pipe(uglify()) // Minify
     .pipe(rename({ suffix: '.min' }))
     .pipe(sourcemaps.write())
-    .pipe(dest('./build/js'))
+    .pipe(dest(defaults.js.dest))
 }
 
 exports.build = series(
