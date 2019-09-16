eb181beab2d12826a604b9d32746fa7d3f2d61d2
Paul Shryock <paul@pshry.com>
2019-09-16 07:37:22 -0400

    Bundle js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 const { src, dest, series, parallel } = require('gulp');
+const sourcemaps = require('gulp-sourcemaps')
 
+/**
+  * Clean files
+  */
 function clean() {
   const del = require('del');
 
@@ -8,6 +12,17 @@ function clean() {
   ]);
 }
 
+/**
+  * Bundle HTML
+  */
+function htmlBundle() {
+  return src('./src/*.html')
+    .pipe(dest('./build'))
+}
+
+/**
+  * Lint CSS
+  */
 function cssLint() {
   const gulpStylelint = require('gulp-stylelint');
 
@@ -22,49 +37,49 @@ function cssLint() {
     }));
 }
 
-function css() {
-  const postcss    = require('gulp-postcss')
-  const sourcemaps = require('gulp-sourcemaps')
+/**
+  * Transpile, concatenate, and minify CSS
+  */
+function cssBundle() {
+  const postcss = require('gulp-postcss')
 
   return src('src/css/*.css')
     .pipe( sourcemaps.init() )
     .pipe( postcss([
-      require('postcss-easy-import'), // Inline @import rules content with extra features
-      require('precss'), // Use Sass-like markup in your CSS
-      require('postcss-node-sass'), // Parse styles with node-sass
-      require('postcss-preset-env'), // Convert modern CSS into something browsers understand
+      require('postcss-easy-import'), // Concatenate
+      require('precss'), // Transpile Sass
+      require('postcss-preset-env'), // Use modern CSS
       require('autoprefixer'), // Add vendor prefixes
-      require('cssnano') // Modern CSS compression
+      require('cssnano') // Minify
       ]) )
     .pipe( sourcemaps.write('.') )
     .pipe( dest('./build/css') )
 }
 
-function jsTranspile(cb) {
-  // body omitted
-  cb();
-}
+/**
+  * Lint JS
+  */
+// function jsLint() {
+// }
 
-function jsBundle(cb) {
-  // body omitted
-  cb();
-}
+function jsBundle() {
+  const concat = require('gulp-concat');
+  const uglify = require('gulp-uglify');
+  const rename = require("gulp-rename");
 
-function jsMinify(cb) {
-  // body omitted
-  cb();
-}
-
-function publish(cb) {
-  // body omitted
-  cb();
+  return src('src/js/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(concat('bundle.js')) // Concatenate
+    .pipe(sourcemaps.write())
+    .pipe(dest('./build/js'))
+    .pipe(uglify()) // Minify
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(sourcemaps.write())
+    .pipe(dest('./build/js'))
 }
 
 exports.build = series(
   clean,
-  parallel(
-    series(cssLint, css),
-    series(jsTranspile, jsBundle, jsMinify)
-  ),
-  publish
+  parallel( htmlBundle, cssLint, /*jsLint*/ ),
+  parallel( cssBundle, jsBundle )
 );
